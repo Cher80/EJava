25e54b5dbfe82b74b6b8d414256a96a8cb516c36
Cher80 <andrew.uglev@gmail.com>
2011-10-07 14:14:40 +0400

    First

---
+++ extraNewGWT/src/my/client/compos/MyComposite2.java
@@ -0,0 +1,37 @@
+package my.client.compos;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class MyComposite2 extends Composite implements IMyCompositeEventHandler {
+	
+	private VerticalPanel panel = new VerticalPanel();
+	private Button myButt1 = new Button("OloloButt2");
+	
+	public MyComposite2() {
+		panel.add(myButt1);
+		
+		myButt1.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				myButt1.setText("Cliknul!");
+				//ComposedEvent myEvent = new ComposedEvent(654);
+				//myEvent.dispatch(new MyCompositeEventHandler());
+				//myEvent.dispatch();
+			}
+		});
+		
+		initWidget(panel);
+	}
+
+	@Override
+	public void onComposed(ComposedEvent event) {
+		// TODO Auto-generated method stub
+		myButt1.setText("BlyaMessaga!");
+		//System.out.println(event.myVar);
+	}
+	
+	
+}
