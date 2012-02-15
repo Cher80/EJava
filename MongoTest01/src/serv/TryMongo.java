4afc28b0ebccf2ccb69f3334510605b8cd4cc048
cher80 <cher80@cher80-MacBook>
2012-02-15 22:08:26 +0300

    s

--- MongoTest01/src/serv/TryMongo.java
+++ MongoTest01/src/serv/TryMongo.java
@@ -12,6 +12,7 @@ import java.util.regex.Pattern;
 
 import javax.print.Doc;
 
+import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.Mongo;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -21,16 +22,21 @@ import com.mongodb.DBCursor;
 import com.mongodb.MongoException;
 
 public class TryMongo {
+	private static Mongo m;
+	private static DB db;
+	
+	
+	
 	
 	public static void main(String [ ] args) throws UnknownHostException, MongoException
 	{
 		System.out.println("w");
 		
 		//Mongo m = new Mongo();
-		Mongo m = new Mongo();
+		m = new Mongo();
 		
 
-		DB db = m.getDB( "test" );
+		db = m.getDB( "test" );
 		
 		/*
 		Set<String> colls = db.getCollectionNames();
@@ -62,28 +68,117 @@ public class TryMongo {
 		
 
 		
-		/*
-		for (int i=3;i<100;i++) {
-			System.out.println(i);
-			BasicDBObject docin = new BasicDBObject();
-			docin.put("cid", 999);
-			docin.put("content", "tuta");
+	
+		
+
+		
+		
+		
+
+	
+		
+		//createTestColl("testColl");
+		//clearTestColl("testColl");
+		//doUpdateComments();
+		//doPopulateComments();
+		doUpdateInc();
+	}
+	
+
+	public static void createTestColl(String collName) {
+		//db.createCollection("xcx");
+		db.createCollection(collName, BasicDBObjectBuilder.start().add("capped", false).add("size", 0).add("max", 0).get());
+	}
+	
+	public static void clearTestColl(String collName) {
+		//DBCollection coll = db.getCollection("testColl");
+		//db.getCollection("testColl").remove(BasicDBObjectBuilder.start().add(null,null).get());
+		db.getCollection(collName).remove(new BasicDBObject());
+
+		//db.createCollection("xcx");
+		//db.createCollection(collName, BasicDBObjectBuilder.start().add("capped", false).add("size", 0).add("max", 0).get());
+	}
+
+	
+	
+	public static void doUpdateIncArray() {
+		
+		DBCollection coll = db.getCollection("testColl");
+		
+		Date myDate1 = new Date();
+		Timestamp timeStampDate1 = new Timestamp(myDate1.getTime());
+		System.out.println(timeStampDate1);
+		BasicDBObject query = new BasicDBObject();
+		
+		//List obj;
+		
+		for (int i=21;i<22;i++) {
 			
-	        
-	        coll.update(new BasicDBObject().append("page_id", 222), 
-	        		//new BasicDBObject().append("page_id", 222),
-	        		new BasicDBObject().append("$push", new BasicDBObject().append("comments", new BasicDBObject().append("cont", "fff"))),
+			
+			BasicDBObject queryup = new BasicDBObject();
+			queryup.append("page_id", i);
+			
+			BasicDBObject inc = new BasicDBObject("$inc", new BasicDBObject("rating", 1));
+			
+			
+			coll.update(
+					queryup,
+					inc,
 	        		true, 
 	        		false);
-
-		} */
-		
-
+		} 
 		
 		
+		Date myDate2 = new Date();
+		Timestamp timeStampDate2 = new Timestamp(myDate2.getTime());
+		System.out.println(timeStampDate2);
+		System.out.println(timeStampDate2.getTime()-timeStampDate1.getTime());
+	}
+	
+	
+	
+	public static void doUpdateInc() {
 		
-
-	DBCollection coll = db.getCollection("testColl");
+		DBCollection coll = db.getCollection("testColl");
+		
+		Date myDate1 = new Date();
+		Timestamp timeStampDate1 = new Timestamp(myDate1.getTime());
+		System.out.println(timeStampDate1);
+		BasicDBObject query = new BasicDBObject();
+		
+		//List obj;
+		
+		for (int i=1;i<100000;i++) {
+			
+			
+			BasicDBObject queryup = new BasicDBObject();
+			queryup.append("page_id", i);
+			
+			BasicDBObject inc = new BasicDBObject("$inc", new BasicDBObject("rating", 1));
+			
+			
+			coll.update(
+					queryup,
+					inc,
+	        		true, 
+	        		false);
+		} 
+		
+		
+		Date myDate2 = new Date();
+		Timestamp timeStampDate2 = new Timestamp(myDate2.getTime());
+		System.out.println(timeStampDate2);
+		System.out.println(timeStampDate2.getTime()-timeStampDate1.getTime());
+	}
+	
+	
+	
+	
+	
+	
+	public static void doUpdateComments() {
+	
+		DBCollection coll = db.getCollection("testColl");
 		
 		Date myDate1 = new Date();
 		Timestamp timeStampDate1 = new Timestamp(myDate1.getTime());
@@ -92,11 +187,11 @@ public class TryMongo {
 		
 		List obj;
 		
-		for (int i=1;i<2;i++) {
+		for (int i=1;i<100;i++) {
 			
 			
 			
-			for (int k=1;k<1000;k++) {
+			for (int k=4;k<1000;k++) {
 				BasicDBObject queryup = new BasicDBObject();
 				queryup.append("page_id", i);
 				BasicDBObject upd = new BasicDBObject();
@@ -106,7 +201,7 @@ public class TryMongo {
 				BasicDBObject comment1 = new BasicDBObject();
 				comment1.append("cid", k);
 				comment1.append("text", "Ololo1");
-
+				comment1.append("rating", 0);
 				//comments.add(comment1);
 				
 				upd.append("$push", new BasicDBObject().append("comments", comment1));
@@ -125,13 +220,17 @@ public class TryMongo {
 		System.out.println(timeStampDate2);
 		System.out.println(timeStampDate2.getTime()-timeStampDate1.getTime());
 		
+			
 		
-		
-		
-		
-		///////////////////  Save  ///////////////////////////////////
+	}
+	
+	
+	
+	///////////////////  Save  ///////////////////////////////////
 
-		/*
+	
+	public static void doPopulateComments() {
+		
 		DBCollection coll = db.getCollection("testColl");
 		
 		Date myDate1 = new Date();
@@ -141,24 +240,27 @@ public class TryMongo {
 		
 		List obj;
 		
-		for (int i=20;i<100000;i++) {
+		for (int i=2;i<100000;i++) {
 			BasicDBObject page = new BasicDBObject();
 			page.put("page_id", i);
-			
+			page.put("rating", 0);
 			//ArrayList x = new ArrayList();
 			ArrayList comments = new ArrayList();
 			
 			BasicDBObject comment1 = new BasicDBObject();
 			comment1.append("cid", 1);
 			comment1.append("text", "Ololo1");
+			comment1.append("rating", 0);
 			
 			BasicDBObject comment2 = new BasicDBObject();
 			comment2.append("cid", 2);
 			comment2.append("text", "Ololo2");
+			comment1.append("rating", 0);
 			
 			BasicDBObject comment3 = new BasicDBObject();
 			comment3.append("cid", 3);
 			comment3.append("text", "Ololo3");
+			comment1.append("rating", 0);
 			
 			comments.add(comment1);
 			comments.add(comment2);
@@ -175,9 +277,8 @@ public class TryMongo {
 		Timestamp timeStampDate2 = new Timestamp(myDate2.getTime());
 		System.out.println(timeStampDate2);
 		System.out.println(timeStampDate2.getTime()-timeStampDate1.getTime());
-		*/
 		
-		
+
 	}
 
 }
